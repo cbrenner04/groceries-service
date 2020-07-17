fabf53a250629a7df9352949041f3ccb7d57c238
cbrenner04 <cbrenner265@gmail.com>
2020-07-17 09:02:44 -0500

    Add all other bulk updates

--- app/controllers/book_list_items_bulk_update_controller.rb
+++ app/controllers/book_list_items_bulk_update_controller.rb
@@ -11,9 +11,23 @@ class BookListItemsBulkUpdateController < ListItemsController
     }
   end
 
-  def update; end
+  def update
+    update_current_items
+    create_new_items if item_params[:move] || item_params[:copy]
+    items.each(&:archive) if item_params[:move]
+
+    head :no_content
+  end
 
   private
+
+  def item_params
+    @item_params ||= params
+                     .require(:book_list_items)
+                     .permit(:author, :clear_author, :category, :clear_category,
+                             :copy, :move, :existing_list_id, :new_list_name,
+                             :update_current_items)
+  end
 
   def items
     @items ||= BookListItem.where(id: params[:item_ids].split(","))
@@ -27,5 +41,66 @@ class BookListItemsBulkUpdateController < ListItemsController
     current_user.write_lists.filter do |list|
       list.type == "BookList" && list.id != params[:list_id].to_i
     end
+  end
+
+  def update_author
+    @update_author ||= item_params[:author] && !item_params[:clear_author]
+  end
+
+  def update_category
+    @update_category ||= item_params[:category] && !item_params[:clear_category]
+  end
+
+  def update_current_author_params
+    return {} unless item_params[:author] || item_params[:clear_author]
+
+    { author: update_author ? item_params[:author] : nil }
+  end
+
+  def update_current_category_params
+    return {} unless item_params[:category] || item_params[:clear_category]
+
+    { category: update_category ? item_params[:category] : nil }
+  end
+
+  def update_current_items
+    return unless item_params[:update_current_items]
+
+    update_params = {}.merge(update_current_author_params,
+                             update_current_category_params)
+    items.update_all(update_params)
+  end
+
+  def author(item)
+    return item[:author] unless item_params[:author] ||
+                                item_params[:clear_author]
+
+    update_author ? item_params[:author] : nil
+  end
+
+  def category(item)
+    return item[:category] unless item_params[:category] ||
+                                  item_params[:clear_category]
+
+    update_category ? item_params[:category] : nil
+  end
+
+  def create_new_items
+    list_id = item_params[:existing_list_id] || create_new_list
+    items.each do |item|
+      BookListItem.create!(user: current_user, book_list_id: list_id,
+                           title: item[:title], author: author(item),
+                           category: category(item))
+    end
+  end
+
+  def create_new_list
+    return unless item_params[:new_list_name]
+
+    new_book_list = BookList.create!(name: item_params[:new_list_name],
+                                     owner: current_user)
+    UsersList.create!(user: current_user, list: new_book_list,
+                      has_accepted: true)
+    new_book_list.id
   end
 end
--- app/controllers/grocery_list_items_bulk_update_controller.rb
+++ app/controllers/grocery_list_items_bulk_update_controller.rb
@@ -11,9 +11,23 @@ class GroceryListItemsBulkUpdateController < ListItemsController
     }
   end
 
-  def update; end
+  def update
+    update_current_items
+    create_new_items if item_params[:move] || item_params[:copy]
+    items.each(&:archive) if item_params[:move]
+
+    head :no_content
+  end
 
   private
+
+  def item_params
+    @item_params ||= params
+                     .require(:grocery_list_items)
+                     .permit(:quantity, :clear_quantity, :category,
+                             :clear_category, :copy, :move, :existing_list_id,
+                             :new_list_name, :update_current_items)
+  end
 
   def items
     @items ||= GroceryListItem.where(id: params[:item_ids].split(","))
@@ -27,5 +41,66 @@ class GroceryListItemsBulkUpdateController < ListItemsController
     current_user.write_lists.filter do |list|
       list.type == "GroceryList" && list.id != params[:list_id].to_i
     end
+  end
+
+  def update_quantity
+    @update_quantity ||= item_params[:quantity] && !item_params[:clear_quantity]
+  end
+
+  def update_category
+    @update_category ||= item_params[:category] && !item_params[:clear_category]
+  end
+
+  def update_current_quantity_params
+    return {} unless item_params[:quantity] || item_params[:clear_quantity]
+
+    { quantity: update_quantity ? item_params[:quantity] : nil }
+  end
+
+  def update_current_category_params
+    return {} unless item_params[:category] || item_params[:clear_category]
+
+    { category: update_category ? item_params[:category] : nil }
+  end
+
+  def update_current_items
+    return unless item_params[:update_current_items]
+
+    update_params = {}.merge(update_current_quantity_params,
+                             update_current_category_params)
+    items.update_all(update_params)
+  end
+
+  def quantity(item)
+    return item[:quantity] unless item_params[:quantity] ||
+                                  item_params[:clear_quantity]
+
+    update_quantity ? item_params[:quantity] : nil
+  end
+
+  def category(item)
+    return item[:category] unless item_params[:category] ||
+                                  item_params[:clear_category]
+
+    update_category ? item_params[:category] : nil
+  end
+
+  def create_new_items
+    list_id = item_params[:existing_list_id] || create_new_list
+    items.each do |item|
+      GroceryListItem.create!(user: current_user, grocery_list_id: list_id,
+                              product: item[:product], quantity: quantity(item),
+                              category: category(item))
+    end
+  end
+
+  def create_new_list
+    return unless item_params[:new_list_name]
+
+    new_grocery_list = GroceryList.create!(name: item_params[:new_list_name],
+                                           owner: current_user)
+    UsersList.create!(user: current_user, list: new_grocery_list,
+                      has_accepted: true)
+    new_grocery_list.id
   end
 end
