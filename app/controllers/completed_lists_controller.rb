f564d4f4537f804c05bc9b9abd1a4c989b152450
Christopher Brenner <cbrenner265@gmail.com>
2020-08-18 13:48:03 -0500

    Update for splitting lists (#21)
    * Update for splitting lists
    
    * Fix github action file

--- app/controllers/completed_lists_controller.rb
+++ app/controllers/completed_lists_controller.rb
@@ -5,6 +5,7 @@ class CompletedListsController < ProtectedRouteController
   # GET /
   def index
     render json: {
+      current_user_id: current_user.id,
       completed_lists: current_user.all_completed_lists,
       current_list_permissions: current_user.current_list_permissions
     }
