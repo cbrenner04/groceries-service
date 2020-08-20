dd2b60ba681f1f3790c3a4caa3423cc33e39b1ec
Christopher Brenner <cbrenner265@gmail.com>
2020-08-20 15:21:25 -0500

    Update security headers (#30)
    * Update security headers
    
    * Lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@
 # no doc
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :reject_methods
 
   def route_not_found
     head :not_found
+  end
+
+  private
+
+  def reject_methods
+    raise ActionController::MethodNotAllowed if %w[TRACK TRACE].include?(request.method)
   end
 end
