c356d435adc4f2909c7b609705e09a69639a78ec
cbrenner04 <cbrenner265@gmail.com>
2022-06-11 16:48:32 -0400

    Fix

--- app/mailers/shared_list_notification_mailer.rb
+++ app/mailers/shared_list_notification_mailer.rb
@@ -5,6 +5,6 @@ class SharedListNotificationMailer < ApplicationMailer
   def notify(sharer_email, sharee_email)
     @sharer = sharer_email
     @url = root_url
-    mail(to: sharee_email, subject: "A list has been shared with you!")
+    mail(to: sharee_email, subject: I18n.t("shared_list_mailer_subject"))
   end
 end
