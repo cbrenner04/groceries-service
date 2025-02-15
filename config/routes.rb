c367615dc6e75265be034769639d523a88be4536
Christopher Brenner <cbrenner265@gmail.com>
2025-02-15 13:22:04 -0600

    Version list item routes (#343)
    * Use versioned routes for list items to prep for updates
    
    * Update tests
    
    * Vulns
    
    * Lint
    
    * Use nested classes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # == Route Map
-#
 
 Rails.application.routes.draw do
+  list_items_routes = lambda do
+    resources :list_items, only: [:create, :edit, :update, :destroy] do
+      collection do
+        resource :bulk_update, only: [:show, :update], controller: "list_items_bulk_update", as: :list_items_bulk_update
+      end
+    end
+  end
   mount_devise_token_auth_for "User", at: "auth", skip: [:invitations]
   as :user do
     patch "/auth/invitation", to: "users/invitations#update"
@@ -10,14 +16,8 @@ Rails.application.routes.draw do
   end
   resources :lists, only: [:index, :show, :create, :edit, :update, :destroy] do
     resource :refresh_list, only: [:create]
-    collection do
-      resource :merge_lists, only: [:create]
-    end
-    resources :list_items, only: [:create, :edit, :update, :destroy] do
-      collection do
-        resource :bulk_update, only: [:show, :update], controller: "list_items_bulk_update", as: :list_items_bulk_update
-      end
-    end
+    collection { resource :merge_lists, only: [:create] }
+    namespace :v1, &list_items_routes
     resources :users_lists, only: [:index, :create, :update, :destroy]
   end
   resources :completed_lists, only: :index
