1cfe58057fb021b05d35f7ffbadd1710ffbdb452
cbrenner04 <cbrenner265@gmail.com>
2025-03-29 13:53:11 -0400

    Remove migrations

--- db/migrate/20250316190505_add_position_to_list_item_field_configurations.rb
+++
@@ -1,5 +0,0 @@
-class AddPositionToListItemFieldConfigurations < ActiveRecord::Migration[8.0]
-  def change
-    add_column :list_item_field_configurations, :position, :integer, null: false
-  end
-end
