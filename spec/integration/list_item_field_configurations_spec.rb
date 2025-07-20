9ab42541d36462422bef1552a1e4cfed4aac2460
cbrenner04 <cbrenner265@gmail.com>
2025-07-20 16:27:15 -0500

    More permissive for item field configurations

--- spec/integration/list_item_field_configurations_spec.rb
+++ spec/integration/list_item_field_configurations_spec.rb
@@ -160,6 +160,150 @@ describe "/list_item_configurations/:list_item_configuration_id/list_item_field_
       end
     end
 
+    context "when user has write permissions on lists using the configuration" do
+      let(:list_using_config) { create(:list, list_item_configuration_id: list_item_configuration.id) }
+
+      before do
+        list_item_configuration.update!(user: other_user)
+        create(:users_list, list: list_using_config, user: user, permissions: "write")
+      end
+
+      describe "GET /" do
+        it "returns list item configuration's list item field configurations" do
+          get list_item_configuration_list_item_field_configurations_path(list_item_configuration.id),
+              headers: auth_params
+
+          response_body = JSON.parse(response.body)
+
+          expect(response).to have_http_status :ok
+          expect(response_body.count).to eq 1
+          expect(response_body.first).to eq(
+            {
+              "archived_at" => nil,
+              "created_at" => list_item_field_configuration[:created_at].iso8601(3),
+              "data_type" => list_item_field_configuration[:data_type],
+              "id" => list_item_field_configuration[:id],
+              "label" => list_item_field_configuration[:label],
+              "list_item_configuration_id" => list_item_configuration[:id],
+              "position" => list_item_field_configuration[:position],
+              "updated_at" => list_item_field_configuration[:updated_at].iso8601(3)
+            }
+          )
+        end
+      end
+
+      describe "POST /" do
+        it "creates a list item field configuration" do
+          post list_item_configuration_list_item_field_configurations_path(list_item_configuration.id),
+               headers: auth_params,
+               params: {
+                 list_item_field_configuration: {
+                   label: "foo",
+                   data_type: "free_text",
+                   position: 1
+                 }
+               }
+
+          new_list_item_field_configuration = list_item_configuration.list_item_field_configurations.last
+
+          expect(response).to have_http_status :ok
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "archived_at" => nil,
+              "created_at" => new_list_item_field_configuration[:created_at].iso8601(3),
+              "data_type" => "free_text",
+              "id" => new_list_item_field_configuration[:id],
+              "label" => "foo",
+              "list_item_configuration_id" => list_item_configuration[:id],
+              "position" => new_list_item_field_configuration[:position],
+              "updated_at" => new_list_item_field_configuration[:updated_at].iso8601(3)
+            }
+          )
+        end
+      end
+
+      describe "PUT /:id" do
+        it "updates the list item field configuration" do
+          put list_item_configuration_list_item_field_configuration_path(
+            list_item_configuration.id,
+            list_item_field_configuration.id
+          ), headers: auth_params,
+             params: {
+               list_item_field_configuration: {
+                 label: "foo",
+                 data_type: "boolean"
+               }
+             }
+
+          list_item_field_configuration.reload
+
+          expect(response).to have_http_status :ok
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "archived_at" => nil,
+              "created_at" => list_item_field_configuration[:created_at].iso8601(3),
+              "data_type" => "boolean",
+              "id" => list_item_field_configuration[:id],
+              "label" => "foo",
+              "list_item_configuration_id" => list_item_configuration[:id],
+              "position" => list_item_field_configuration[:position],
+              "updated_at" => list_item_field_configuration[:updated_at].iso8601(3)
+            }
+          )
+        end
+      end
+
+      describe "DELETE /:id" do
+        it "archives the list item field configuration" do
+          expect(list_item_field_configuration.archived_at).to be_falsy
+
+          delete list_item_configuration_list_item_field_configuration_path(
+            list_item_configuration.id,
+            list_item_field_configuration.id
+          ), headers: auth_params
+
+          list_item_field_configuration.reload
+
+          expect(response).to have_http_status :no_content
+          expect(list_item_field_configuration.archived_at).to be_truthy
+        end
+      end
+    end
+
+    context "when user has read permissions on lists using the configuration" do
+      let(:list_using_config) { create(:list, list_item_configuration_id: list_item_configuration.id) }
+
+      before do
+        list_item_configuration.update!(user: other_user)
+        create(:users_list, list: list_using_config, user: user, permissions: "read")
+      end
+
+      describe "GET /" do
+        it "returns 403" do
+          get list_item_configuration_list_item_field_configurations_path(list_item_configuration.id),
+              headers: auth_params
+
+          expect(response).to have_http_status :forbidden
+        end
+      end
+
+      describe "POST /" do
+        it "returns 403" do
+          post list_item_configuration_list_item_field_configurations_path(list_item_configuration.id),
+               headers: auth_params,
+               params: {
+                 list_item_field_configuration: {
+                   label: "foo",
+                   data_type: "free_text",
+                   position: 1
+                 }
+               }
+
+          expect(response).to have_http_status :forbidden
+        end
+      end
+    end
+
     context "when user is owner of list item configuration" do
       describe "GET /" do
         it "returns list item configuration's list item field configurations" do
