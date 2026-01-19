775e3acd43a166b88c5add9e3d6173c7bf08e229
cbrenner04 <cbrenner265@gmail.com>
2026-01-18 21:35:52 -0600

    Include all list types in list type items spec

--- spec/models/list_types_spec.rb
+++ spec/models/list_types_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 
 RSpec.describe List do
   it "instantiates the STI list classes" do
+    expect(create(:book_list)).to be_a(BookList)
+    expect(create(:grocery_list)).to be_a(GroceryList)
     expect(create(:music_list)).to be_a(MusicList)
     expect(create(:simple_list)).to be_a(SimpleList)
     expect(create(:to_do_list)).to be_a(ToDoList)
