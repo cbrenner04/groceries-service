5ea33cde281042343141e2b810b01b2806278aef
cbrenner04 <cbrenner265@gmail.com>
2026-01-18 23:27:24 -0600

    Whitespace whatever

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,57 +14,47 @@ end
 # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
 def create_list_item_configurations(user)
   # Book list configuration
-  book_config = user.list_item_configurations.create!(
-    name: "book list template"
-  )
+  book_config = user.list_item_configurations.create!(name: "book list template")
   book_config.list_item_field_configurations.create!([
-                                                       { label: "author", data_type: "free_text", position: 1 },
-                                                       { label: "title", data_type: "free_text", position: 2 },
-                                                       { label: "number_in_series", data_type: "number", position: 3 },
-                                                       { label: "category", data_type: "free_text", position: 4 },
-                                                       { label: "read", data_type: "boolean", position: 5 }
-                                                     ])
+    { label: "author", data_type: "free_text", position: 1 },
+    { label: "title", data_type: "free_text", position: 2 },
+    { label: "number_in_series", data_type: "number", position: 3 },
+    { label: "category", data_type: "free_text", position: 4 },
+    { label: "read", data_type: "boolean", position: 5 }
+  ])
 
   # Grocery list configuration
-  grocery_config = user.list_item_configurations.create!(
-    name: "grocery list template"
-  )
+  grocery_config = user.list_item_configurations.create!(name: "grocery list template")
   grocery_config.list_item_field_configurations.create!([
-                                                          { label: "product", data_type: "free_text", position: 1 },
-                                                          { label: "quantity", data_type: "free_text", position: 2 },
-                                                          { label: "category", data_type: "free_text", position: 3 }
-                                                        ])
+    { label: "product", data_type: "free_text", position: 1 },
+    { label: "quantity", data_type: "free_text", position: 2 },
+    { label: "category", data_type: "free_text", position: 3 }
+  ])
 
   # Music list configuration
-  music_config = user.list_item_configurations.create!(
-    name: "music list template"
-  )
+  music_config = user.list_item_configurations.create!(name: "music list template")
   music_config.list_item_field_configurations.create!([
-                                                        { label: "title", data_type: "free_text", position: 1 },
-                                                        { label: "artist", data_type: "free_text", position: 2 },
-                                                        { label: "album", data_type: "free_text", position: 3 },
-                                                        { label: "category", data_type: "free_text", position: 4 }
-                                                      ])
+    { label: "title", data_type: "free_text", position: 1 },
+    { label: "artist", data_type: "free_text", position: 2 },
+    { label: "album", data_type: "free_text", position: 3 },
+    { label: "category", data_type: "free_text", position: 4 }
+  ])
 
   # TODO: list configuration
-  todo_config = user.list_item_configurations.create!(
-    name: "todo list template"
-  )
+  todo_config = user.list_item_configurations.create!(name: "todo list template")
   todo_config.list_item_field_configurations.create!([
-                                                       { label: "task", data_type: "free_text", position: 1 },
-                                                       { label: "assignee", data_type: "free_text", position: 2 },
-                                                       { label: "due_by", data_type: "date_time", position: 3 },
-                                                       { label: "category", data_type: "free_text", position: 4 }
-                                                     ])
+    { label: "task", data_type: "free_text", position: 1 },
+    { label: "assignee", data_type: "free_text", position: 2 },
+    { label: "due_by", data_type: "date_time", position: 3 },
+    { label: "category", data_type: "free_text", position: 4 }
+  ])
 
   # Simple list configuration
-  simple_config = user.list_item_configurations.create!(
-    name: "simple list template"
-  )
+  simple_config = user.list_item_configurations.create!(name: "simple list template")
   simple_config.list_item_field_configurations.create!([
-                                                         { label: "content", data_type: "free_text", position: 1 },
-                                                         { label: "category", data_type: "free_text", position: 2 }
-                                                       ])
+      { label: "content", data_type: "free_text", position: 1 },
+      { label: "category", data_type: "free_text", position: 2 }
+    ])
 
   [book_config, grocery_config, music_config, todo_config, simple_config]
 end
@@ -129,12 +119,9 @@ end
 
 # Sample data for different list types
 book_sample_data = [
-  { "author" => "J.R.R. Tolkien", "title" => "The Hobbit", "number_in_series" => "1", "category" => "Fantasy",
-    "read" => "false" },
-  { "author" => "George R.R. Martin", "title" => "A Game of Thrones", "number_in_series" => "1",
-    "category" => "Fantasy", "read" => "true" },
-  { "author" => "Patrick Rothfuss", "title" => "The Name of the Wind", "number_in_series" => "1",
-    "category" => "Fantasy", "read" => "false" }
+  { "author" => "J.R.R. Tolkien", "title" => "The Hobbit", "number_in_series" => "1", "category" => "Fantasy", "read" => "false" },
+  { "author" => "George R.R. Martin", "title" => "A Game of Thrones", "number_in_series" => "1", "category" => "Fantasy", "read" => "true" },
+  { "author" => "Patrick Rothfuss", "title" => "The Name of the Wind", "number_in_series" => "1", "category" => "Fantasy", "read" => "false" }
 ]
 
 grocery_sample_data = [
@@ -145,20 +132,15 @@ grocery_sample_data = [
 ]
 
 music_sample_data = [
-  { "title" => "Bohemian Rhapsody", "artist" => "Queen", "album" => "A Night at the Opera", "category" => "Rock",
-    "purchased" => "false" },
-  { "title" => "Hotel California", "artist" => "Eagles", "album" => "Hotel California", "category" => "Rock",
-    "purchased" => "true" },
+  { "title" => "Bohemian Rhapsody", "artist" => "Queen", "album" => "A Night at the Opera", "category" => "Rock", "purchased" => "false" },
+  { "title" => "Hotel California", "artist" => "Eagles", "album" => "Hotel California", "category" => "Rock", "purchased" => "true" },
   { "title" => "Imagine", "artist" => "John Lennon", "album" => "Imagine", "category" => "Pop", "purchased" => "false" }
 ]
 
 todo_sample_data = [
-  { "task" => "Buy groceries", "assignee_id" => foo.id, "due_by" => "2024-12-25T18:00:00Z", "category" => "Shopping",
-    "completed" => "false" },
-  { "task" => "Call dentist", "assignee_id" => bar.id, "due_by" => "2024-12-20T17:00:00Z", "category" => "Health",
-    "completed" => "true" },
-  { "task" => "Review project proposal", "assignee_id" => foo.id, "due_by" => "2024-12-22T16:00:00Z",
-    "category" => "Work", "completed" => "false" }
+  { "task" => "Buy groceries", "assignee_id" => foo.id, "due_by" => "2024-12-25T18:00:00Z", "category" => "Shopping", "completed" => "false" },
+  { "task" => "Call dentist", "assignee_id" => bar.id, "due_by" => "2024-12-20T17:00:00Z", "category" => "Health", "completed" => "true" },
+  { "task" => "Review project proposal", "assignee_id" => foo.id, "due_by" => "2024-12-22T16:00:00Z", "category" => "Work", "completed" => "false" }
 ]
 
 simple_sample_data = [
